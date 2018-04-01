2d432fbb4ee126589f269501cc7d10167de6f108
mrchen2015 <562638230@qq.com>
2018-04-01 13:53:31 +0800

    添加百度地图sdk,显示mapview

---
+++ app/src/main/java/xyz/chenqun/github/threadtest/Main2Activity.java
@@ -0,0 +1,143 @@
+package xyz.chenqun.github.threadtest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main2Activity extends AppCompatActivity {
+
+    public LocationClient mLocationClient;
+
+    private TextView positionText;
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new BDAbstractLocationListener() {
+            @Override
+            public void onReceiveLocation(BDLocation bdLocation) {
+                StringBuffer currentPosition = new StringBuffer();
+                currentPosition.append("纬度:").append(bdLocation.getLatitude()).append("\n");
+                currentPosition.append("经度:").append(bdLocation.getLongitude()).append("\n");
+                currentPosition.append("国家:").append(bdLocation.getCountry()).append("\n");
+                currentPosition.append("省:").append(bdLocation.getProvince()).append("\n");
+                currentPosition.append("市:").append(bdLocation.getCity()).append("\n");
+                currentPosition.append("区:").append(bdLocation.getDistrict()).append("\n");
+                currentPosition.append("街道:").append(bdLocation.getStreet()).append("\n");
+                currentPosition.append("定位方式:");
+                if (bdLocation.getLocType()==BDLocation.TypeGpsLocation){
+                    currentPosition.append("GPS");
+                }else if(bdLocation.getLocType()==BDLocation.TypeNetWorkLocation){
+                    currentPosition.append("网络");
+                }
+                positionText.setText(currentPosition);
+            }
+        });
+
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main2);
+        mapView= (MapView)findViewById(R.id.bmapView);
+
+        positionText =(TextView) findViewById(R.id.position_text);
+        List<String> permissionList = new ArrayList<String>();
+        if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(Main2Activity.this,permissions,1);
+        }else {
+            requestLocation();
+        }
+    }
+
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setScanSpan(5000);
+        option.setIsNeedAddress(true);
+        //option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);
+        mLocationClient.setLocOption(option);
+    }
+    private void requestLocation(){
+        mLocationClient.start();
+        initLocation();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mLocationClient.stop();
+
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0){
+                    for (int result:grantResults){
+                        if (result!=PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"必须同意全部权限才能使用本程序",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                }else {
+                    Toast.makeText(this,"发生未知错误",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
--- app/src/main/java/xyz/chenqun/github/threadtest/MainActivity.java
+++ app/src/main/java/xyz/chenqun/github/threadtest/MainActivity.java
@@ -15,96 +15,96 @@ import android.widget.TextView;
 import xyz.chenqun.github.cqlibrary.CustomUtil;
 import xyz.chenqun.github.threadtest.services.MyService;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
-    public static final int UPDATE_TEXT = 1;
-    private TextView text;
-    //异步消息处理
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg){
-            switch (msg.what){
-                case UPDATE_TEXT:
-                    text.setText("Nice to meet you ");
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-
-    private MyService.DownloadBinder downloadBinder;
-
-    private ServiceConnection connection = new ServiceConnection(){
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            downloadBinder = (MyService.DownloadBinder)service;
-            downloadBinder.startDownload();
-            downloadBinder.getProgress();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-
-        }
-    };
+//    public static final int UPDATE_TEXT = 1;
+//    private TextView text;
+//    //异步消息处理
+//    private Handler handler = new Handler(){
+//        @Override
+//        public void handleMessage(Message msg){
+//            switch (msg.what){
+//                case UPDATE_TEXT:
+//                    text.setText("Nice to meet you ");
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+//    };
+//
+//    private MyService.DownloadBinder downloadBinder;
+//
+//    private ServiceConnection connection = new ServiceConnection(){
+//        @Override
+//        public void onServiceConnected(ComponentName name, IBinder service) {
+//            downloadBinder = (MyService.DownloadBinder)service;
+//            downloadBinder.startDownload();
+//            downloadBinder.getProgress();
+//        }
+//
+//        @Override
+//        public void onServiceDisconnected(ComponentName name) {
+//
+//        }
+//    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView)findViewById(R.id.textview) ;
-        Button button = (Button)findViewById(R.id.change_text);
-        button.setOnClickListener(this);
-
-        Button startService = (Button)findViewById(R.id.start_service);
-        Button stopService = (Button)findViewById(R.id.stop_service);
-        startService.setOnClickListener(this);
-        stopService.setOnClickListener(this);
-
-        Button bindService = (Button)findViewById(R.id.bind_service);
-        Button unbindService = (Button)findViewById(R.id.unbind_service);
-        bindService.setOnClickListener(this);
-        unbindService.setOnClickListener(this);
+//        text = (TextView)findViewById(R.id.textview) ;
+//        Button button = (Button)findViewById(R.id.change_text);
+//        button.setOnClickListener(this);
+//
+//        Button startService = (Button)findViewById(R.id.start_service);
+//        Button stopService = (Button)findViewById(R.id.stop_service);
+//        startService.setOnClickListener(this);
+//        stopService.setOnClickListener(this);
+//
+//        Button bindService = (Button)findViewById(R.id.bind_service);
+//        Button unbindService = (Button)findViewById(R.id.unbind_service);
+//        bindService.setOnClickListener(this);
+//        unbindService.setOnClickListener(this);
 
         //测试自定义库是否导入成功
         //CustomUtil.HelloWorld();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.change_text:
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        //aa
-                        text.setText("Nice to meet you");
-                        Message message = new Message();
-                        message.what = UPDATE_TEXT;
-                        handler.sendMessage(message);
-                    }
-                }).start();
-                break;
-            case R.id.start_service:
-                Intent startIntent = new Intent(this, MyService.class);
-                startService(startIntent);
-                break;
-            case R.id.stop_service:
-                Intent stopIntent = new Intent(this, MyService.class);
-                stopService(stopIntent);
-                break;
-            case R.id.bind_service:
-                Intent bindIntent = new Intent(this, MyService.class);
-                bindService(bindIntent,connection,BIND_AUTO_CREATE);//绑定服务
-                break;
-            case R.id.unbind_service:
-                Intent unbindIntent = new Intent(this, MyService.class);
-                unbindService(connection);//解绑服务
-                break;
-            default:
-                break;
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.change_text:
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        //aa
+//                        text.setText("Nice to meet you");
+//                        Message message = new Message();
+//                        message.what = UPDATE_TEXT;
+//                        handler.sendMessage(message);
+//                    }
+//                }).start();
+//                break;
+//            case R.id.start_service:
+//                Intent startIntent = new Intent(this, MyService.class);
+//                startService(startIntent);
+//                break;
+//            case R.id.stop_service:
+//                Intent stopIntent = new Intent(this, MyService.class);
+//                stopService(stopIntent);
+//                break;
+//            case R.id.bind_service:
+//                Intent bindIntent = new Intent(this, MyService.class);
+//                bindService(bindIntent,connection,BIND_AUTO_CREATE);//绑定服务
+//                break;
+//            case R.id.unbind_service:
+//                Intent unbindIntent = new Intent(this, MyService.class);
+//                unbindService(connection);//解绑服务
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 }
